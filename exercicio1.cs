0542835b53b09b90ccda4a1e85e1157553b55e45
Jessicasantiago-ux <nascimentoadalziza@gmail.com>
2025-03-21 11:59:23 +0000

    Add files via upload

---
+++ exercicio1.cs
@@ -0,0 +1,34 @@
+﻿using System; // Importação do namespace padrão para entrada/saída
+
+public class Exercicio1
+{
+    public static void Main(string[]args)
+    {
+        Console.WriteLine("Digite dois números inteiros.");
+
+        // declarar variaveis
+        int numero1 = 10;
+        int numero2 = 20;
+
+        // Realiza as operações
+        int soma = numero1 + numero2;
+        int subtracao = numero1 - numero2;
+        int multiplicacao = numero1 * numero2;
+        int divisao = numero1 / numero2;
+        int resto = numero1 % numero2;
+
+
+        // Exibe os resultados
+        Console.WriteLine("Soma: " + soma);
+        Console.WriteLine("Subtração: " + subtracao);
+        Console.WriteLine("Multiplicação: " + multiplicacao);
+        Console.WriteLine("Divisão: " + divisao);
+        Console.WriteLine("Resto: " + resto);
+
+
+
+    }
+
+
+}
+

9727f3603b2a7ac1e4fbc2c113bf814e70f90598
Jessicasantiago-ux <nascimentoadalziza@gmail.com>
2025-03-28 12:28:48 +0000

    Add files via upload

---
+++ exercicio4.cs
@@ -0,0 +1,27 @@
+﻿  using System;
+
+ public class Exercicio4
+ {
+     public static void Main(string[] args)
+     {
+
+        int idade = 68; 
+
+         if ( idade < 18 )
+         {
+            Console.WriteLine("És menor de idade ");
+
+         }
+         else if(idade >=18 && idade <= 65)
+         {
+            Console.WriteLine("És adulto e solteiro ou és adulto e casado");
+
+         }
+         else
+         {
+            Console.WriteLine("És sénior");
+         }
+     }
+        
+ }    
+

bdf63957e647f467c34bd0edad240119ee3b9793
Jessicasantiago-ux <nascimentoadalziza@gmail.com>
2025-04-23 17:13:03 +0100

    Add files via upload

---
+++ Exercicio6lp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+ public  class Exercicio6
+ {
+     public static void Main(string[] args)
+     {
+        Console.WriteLine("Digite a sua nota");
+
+        int nota = 80;
+
+        if (nota >= 90)
+        {
+            Console.WriteLine("A");// Excelente
+        }
+        else if (nota >= 80 && nota <= 89)
+        {
+            Console.WriteLine("B");// Muito bom
+        }
+        else if (nota >= 70 && nota <= 79)
+        {
+            Console.WriteLine("C");//  Bom
+        }
+        else if (nota >= 60 && nota <= 69)
+        {
+            Console.WriteLine("D"); // Precisa melhorar
+        }
+        else
+        {
+            Console.WriteLine("F - reprovado");
+        }
+            
+        
+
+     }
+ }
+

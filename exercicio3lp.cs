adb67ef43ffe537d82333990d338d1f2bdc3bb3f
Jessicasantiago-ux <nascimentoadalziza@gmail.com>
2025-04-24 12:57:13 +0100

    Add files via upload

---
+++ exercicio3lp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Exercico3
+{
+    public static void Main(string[] args)
+    {
+
+        Console.WriteLine("Digite a sua idade");
+
+        bool isEstudante = true;
+
+        int idade = 18;
+
+        if (idade >= 18 && isEstudante)
+        {
+            Console.WriteLine("É um estudante adulto");
+        }
+
+    }
+}

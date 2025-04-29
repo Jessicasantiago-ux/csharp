442f48f5a99f8f1063085ffc93a7137d04369e13
Jessicasantiago-ux <nascimentoadalziza@gmail.com>
2025-04-29 16:43:57 +0100

    Add files via upload

---
+++ Exercicio8lp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Exercicio8
+{
+    public static void Main(string[]args)
+    {
+
+        Console.WriteLine("Números pares entre 1 e 20");
+
+        for ( int i = 1; i <= 20; i++ )
+        {
+            if (i % 2 == 0)
+            {
+                Console.WriteLine(i);
+            }
+                
+        }
+        
+    }
+}

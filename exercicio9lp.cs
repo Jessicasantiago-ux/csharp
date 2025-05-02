0cb3c12a9cbbeed151785f04d291b9d0015ca5e4
Jessicasantiago-ux <nascimentoadalziza@gmail.com>
2025-05-02 13:01:06 +0100

    Add files via upload

---
+++ exercicio9lp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Exercicio9
+{
+    public static void Main(string[]args)
+    {
+
+        int soma = 0;
+        {
+            Console.WriteLine(" calculo da soma dos números ímpares entre 1 e 50 ");
+        }
+        for (int i =1; i <= 50; i++)
+        {
+            if ( i % 2 != 0)
+            {
+                soma += i;
+            }
+        }
+        Console.WriteLine("A soma dos números ímpares entre 1 e 50 é :" + soma);
+        
+    }
+}

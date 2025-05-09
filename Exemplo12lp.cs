dc88be85cb9e5b6efbd58cce7f73ec56020323bf
Jessicasantiago-ux <nascimentoadalziza@gmail.com>
2025-05-09 12:35:20 +0100

    Add files via upload

---
+++ Exemplo12lp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Exercicio12
+{
+    public static void Main(string[]args) 
+    {
+
+        int i = 1;
+
+        do
+        {
+            Console.WriteLine(i);
+            i++;
+        } while (i <= 10);
+        
+    }
+}

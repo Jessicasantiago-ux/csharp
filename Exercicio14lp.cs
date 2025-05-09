cfe8d07a18d8eac2ecda963d277bd5bcb1965034
Jessicasantiago-ux <nascimentoadalziza@gmail.com>
2025-05-09 13:09:37 +0100

    Add files via upload

---
+++ Exercicio14lp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class Exercicio14
+{
+    public static void Main(string[] args)
+    {
+
+        int resultado = MaiorMenor(10, 20);
+
+        Console.WriteLine("o maior numero dos " + "dois: " + resultado);
+    }
+
+    static int  MaiorMenor(int n1, int n2)
+    {
+        if (n1 > n2)
+        {
+            return n1; 
+        }
+        else
+        {
+            return n2;
+        }
+   
+    }
+     
+
+         
+
+
+}
+  
+  

c5bfda929e0414936b6df5db6089aab3f9271ba4
Jessicasantiago-ux <nascimentoadalziza@gmail.com>
2025-05-09 12:23:37 +0100

    Add files via upload

---
+++ exercicio11lp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Exercicio11
+{
+    public static void Main(string[] args)
+    {
+
+        int soma = 0;
+        int numero = 1;
+
+        do
+        {
+            soma += numero;
+            numero++;
+        } while (soma < 100);
+
+        Console.WriteLine("A soma acumulada é :" + soma);
+        Console.WriteLine("O último número adicionado foi :" + (numero - 1));
+
+    }
+}

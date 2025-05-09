5f7b33f796cc7db5bee5d3957788a6b06a98a4d0
Jessicasantiago-ux <nascimentoadalziza@gmail.com>
2025-05-09 11:52:49 +0100

    Add files via upload

---
+++ Exercicio10lp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Exercicio10
+{
+    public static void Main(string[]args)
+    {
+
+        int contador = 1;
+        int numero = 3;
+
+        while (contador <= 10)
+        {
+            Console.WriteLine(numero * contador);
+            contador ++;
+        }
+        
+        
+    }
+}

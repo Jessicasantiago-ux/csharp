b1f736eedd36e9f22d9c74b5bea7915bc317a944
Jessicasantiago-ux <nascimentoadalziza@gmail.com>
2025-05-14 10:25:31 +0100

    Add files via upload

---
+++ Exercicio16lp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Exercicio16
+{
+    public static void Main(string[]args)
+    {
+        int numero = 10;
+
+        int quadrado = numero * numero;
+
+        if( numero % 2 == 0)
+        {
+            Console.WriteLine("O numero indicado é par e o seu quadrado é " + quadrado);
+        }
+        else
+        {
+            Console.WriteLine("O numero é ímpar.");
+        }
+    }
+}

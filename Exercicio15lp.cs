663fcbb8d51f827e5067cd3c172f280d6d5827f4
Jessicasantiago-ux <nascimentoadalziza@gmail.com>
2025-05-14 10:10:43 +0100

    Add files via upload

---
+++ Exercicio15lp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Exercicio15
+{
+    public static void Main(string[]args)
+    {
+        int numero = 8;
+        if(numero % 2 == 0)
+        {
+            Console.WriteLine("O numro é par");
+        }
+        else
+        {
+            Console.WriteLine("O numero é ímpar");
+        }
+    }
+}
